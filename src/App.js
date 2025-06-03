2342564e17ebd1f76ad0c2c2af0d24e3b6883c30
noahcha0907 <noahcha@yahoo.com>
2025-06-03 17:27:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import VasariPlatform from './components/VasariPlatform';
+import './index.css';
 
+function App() {
+  return (
+    <div className="App">
+      <VasariPlatform />
+    </div>
+  );
+}
+
+export default App;
